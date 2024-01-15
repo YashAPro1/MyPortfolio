ad7508775da23466edb80be52468b7d3009449bf
YashAPro1 <rekhasd01000001@gmail.com>
2024-01-16 00:07:39 +0530

    Updated new blog

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -5,7 +5,7 @@ import Auto from '../assets/automated_mail.jpg';
 import Card from './Cards';
 import mlimg from "../assets/ml.jpeg"
 import cicd from "../assets/cicd.jpeg"
-
+import rlhf from "../assets/rlhf.jpeg"
 import {motion} from 'framer-motion';
 
 
@@ -25,6 +25,11 @@ export default function Blogs() {
                     name='Automating With CI/CD Pipeline'
                     imgsrc={cicd}
                     ssrc='https://blogwithdubey.hashnode.dev/automating-with-cicd-pipeline'
+                />
+                <Card
+                    name='Reinforcement Learning with Human Feedback'
+                    imgsrc={rlhf}
+                    ssrc='https://blogwithdubey.hashnode.dev/reinforcement-learning-with-human-feedback-rlhf'
                 />
             </div>
         </section>
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,12 +6,12 @@ export default function Cards(props) {
     <motion.div 
     initial={{opacity:0, x: 0,y:100}} 
     whileInView={{opacity:1, x: 0,y:0}} 
-    transition={{duration:0.5}} 
+    transition={{duration:0.5}}
     >
     <a href={props.ssrc} target='_blank'>
       <motion.div  className="box" >
 
-        <img src={props.imgsrc} className="img1" alt="" style={{width:'100%',height:'300px'}}/>
+        <img src={props.imgsrc} className="img1" alt="" style={{width:'100%'}}/>
         <h3 className= "boxh">{props.name}</h3>
 
       </motion.div >
